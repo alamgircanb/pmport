7722f2cbfb6baa5b8c593f57862eef59a3df45e5
Md Alamgir Hossain,MBA,PMP,CSM,ITILv5 <hossain8078@saskpolytech.ca>
2026-09-22 12:05:59 -0600

    Add PowerShell coursework entry to project list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -99,7 +99,8 @@ export class AppComponent {
         { title: 'Data Science Basics Course Project', repositoryUrl: 'https://github.com/alamgircanb/Business-Analysis-Data-Analytics-Intelligence/tree/main/DataScienceBasicsProject', priority: 5},
         { title: 'Final Project_SSIS_SQL_PowerBI_Final (FlatFile-SSIS-SQL-PowerBI)', repositoryUrl: 'https://github.com/alamgircanb/Business-Analysis-Data-Analytics-Intelligence/tree/main/Final%20Project_SSIS_SQL_PowerBI_Final', priority: 6},
         { title: 'Database Management (MongoDB)', repositoryUrl: 'https://github.com/alamgircanb/Business-Analysis-Data-Analytics-Intelligence/tree/main/MongoDB', priority: 7},
-        { title: 'Database Management Basics (SQL)', repositoryUrl: 'https://github.com/alamgircanb/Business-Analysis-Data-Analytics-Intelligence/tree/main/SQL_Basics', priority: 8}
+        { title: 'Database Management Basics (SQL)', repositoryUrl: 'https://github.com/alamgircanb/Business-Analysis-Data-Analytics-Intelligence/tree/main/SQL_Basics', priority: 8},
+         { title: 'PowerShell_CourseWork', repositoryUrl: 'https://github.com/alamgircanb/Business-Analysis-Data-Analytics-Intelligence/tree/main/PowerShell_myWork', priority: 9}
       ],
       image: 'power-bi-project-presentation.png',
       imageAlt: 'Alamgir presenting a project progress dashboard to his team'
